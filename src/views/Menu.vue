712628277f9b06b9cb872916f29d53531b58e7c3
Sunanta Singka <sunanta.gift@mail.kmutt.ac.th>
2021-04-16 21:43:50 +0700

    [update] 62130500098 Menu

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -22,6 +22,52 @@
         <p>15 Bath</p>
       </div>
     </div>
+
+    <div class="grid grid-cols-4 pt-10 px-10" >
+      <div>
+        <img src="../assets/dumplings/Crab-Dumpling.jpeg">
+        <p class="font-semibold">Crab Dumpling</p>
+        <p>20 Bath</p>
+      </div>
+      <div>
+        <img src="../assets/dumplings/Pork-Dumpling.jpeg">
+        <p class="font-semibold">Pork Dumpling</p>
+        <p>20 Bath</p>
+      </div>
+      <div>
+        <img src="../assets/dumplings/Shrimp-Dumpling.jpeg">
+        <p class="font-semibold">Shrimp Dumpling</p>
+        <p>20 Bath</p>
+      </div>
+      <div>
+        <img src="../assets/dumplings/Shrimp-Pork-Dumpling.jpeg">
+        <p class="font-semibold">Shrimp Pork Dumpling</p>
+        <p>20 Bath</p>
+      </div>
+    </div>
+
+    <div class="grid grid-cols-4 pt-10 px-10" >
+      <div>
+        <img src="../assets/wonton/pork-wonton.jpeg">
+        <p class="font-semibold">Pork Wonton</p>
+        <p>20 Bath</p>
+      </div>
+      <div>
+        <img src="../assets/wonton/prawn-wonton.jpeg">
+        <p class="font-semibold">Prawn Wonton</p>
+        <p>20 Bath</p>
+      </div>
+      <div>
+        <img src="../assets/wonton/crab-wonton.jpeg">
+        <p class="font-semibold">Crab Wonton</p>
+        <p>20 Bath</p>
+      </div>
+      <div>
+        <img src="../assets/wonton/fish-wonton.jpeg">
+        <p class="font-semibold">Fish Wonton</p>
+        <p>20 Bath</p>
+      </div>
+    </div>
   </div>
 </template>
 
